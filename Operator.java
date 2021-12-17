9b1d4f45cf0a81b0e18cc67e7bca865fa5b55522
ryotagonG <87141681+ryotagonG@users.noreply.github.com>
2021-12-17 17:58:12 +0900

    条件分岐

---
+++ Operator.java
@@ -0,0 +1,54 @@
+class Operator {
+  public static void main(String[] args) {
+    //算術演算子
+    // int x = 10;
+    // int y = 2;
+
+    // System.out.println(x+y);
+    // System.out.println(x-y);
+    // System.out.println(x*y);
+    // System.out.println(x/y);
+    // System.out.println(x%y);
+
+    //関係演算子
+    // int x = 10;
+    // int y = 2;
+    // int z = 10;
+
+    // System.out.println(x >= y);
+    // System.out.println(x <= y);
+    // System.out.println(x <= z);
+
+    //等価
+    // System.out.println(x == y);
+    // System.out.println(x != y);
+
+    //論理演算子
+    // int x = 8;
+    // int y = 3;
+
+    // //かつ
+    // System.out.println(x >= 5 && x <= 10);
+    // System.out.println(y >= 5 && y <= 10);
+    // //または
+    // System.out.println(x == 3 || y == 3);
+    // System.out.println(x == 1 || y == 1);
+
+    //代入演算子
+    // int x = 8;
+    // int y = 12;
+    // int z = 20;
+
+    // System.out.println(x += 10);
+    // System.out.println(z += y);
+
+    //インクリメントとデクリメント
+    int x = 8;
+    int y = 8;
+    x++;
+    y--;
+
+    System.out.println(x);
+    System.out.println(y);
+  }
+}
