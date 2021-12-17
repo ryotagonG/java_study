4339e257329909765a161abbc18138ca8a53027c
ryotagonG <87141681+ryotagonG@users.noreply.github.com>
2021-12-17 18:38:56 +0900

    メソッド

---
+++ Method.java
@@ -0,0 +1,9 @@
+class Method {
+  public static void main(String[] args) {
+  //public 他のクラスからアクセス可能なアクセス修飾子
+  //static クラスから直接呼び出し可能な修飾子
+  //void 戻り値がないメソッドの時に記述
+  //main javaにかならず必要なメソッド名
+  //(String[] args) main関数の場合には一般的な書き方
+  }
+}
